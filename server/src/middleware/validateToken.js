146ae19c4474f2c63814464090dd5033d9a5c97c
Johnson179 <j179988143412@gmail.com>
2024-06-29 16:25:59 +0800

    add jsconfig.json

--- server/src/middleware/validateToken.js
+++ server/src/middleware/validateToken.js
@@ -1,16 +1,20 @@
 import jwt from "jsonwebtoken";
+import Env from "../utils/constants/Env.js";
 
 const validateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; 
+  const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({status:401, message:"Token Unauthorized"});
+    return res.status(401).json({ status: 401, message: "Token Unauthorized" });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403); // If token is not valid, return Forbidden
+  jwt.verify(token, Env.JWT_SECRET, (err, user) => {
+    if (err) {
+      // Token is not valid
+      return res.status(401).json({ status: 401, message: "Token Forbidden" });
+    }
     req.user = user; // Add the user payload to the request object
-    next(); // Proceed to the next middleware or route handler
+    next();
   });
 };
